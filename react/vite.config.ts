c9b68b78d167e531dc6f991e6f152b1beda3d3a7
Abhishek Yadav <abhishekisonit@gmail.com>
2023-07-23 00:56:06 +0530

    Progress of setup. started working on main.tsx

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      find: "@",
+      replacement: fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  base: "/front-end-challenges/react/dist",
+});
